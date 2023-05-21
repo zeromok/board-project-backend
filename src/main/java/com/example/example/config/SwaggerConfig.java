045069547ce0ebe2d55b80a864475dc082abc5d1
Daniel <byoung.mokk@gmail.com>
2023-05-21 15:39:37 +0900

    Docs: SwaggerConfig 설정

---
+++ src/main/java/com/example/example/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.example.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String springdocVersion) {
+        Info info = new Info()
+                .title("board-project")
+                .version(springdocVersion)
+                .description("INIT");
+        return new OpenAPI().info(info);
+    }
+}
